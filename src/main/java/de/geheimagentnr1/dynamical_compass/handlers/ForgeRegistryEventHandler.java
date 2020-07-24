f448e819dffe1d1787ee5ac5ff9bbf1c369c99db
GeheimagentNr1 <GeheimeLache@hotmail.com>
2020-07-25 00:50:34 +0200

    Fixed Problems with none existing Method for Command registration.

--- src/main/java/de/geheimagentnr1/dynamical_compass/handlers/ForgeRegistryEventHandler.java
+++ src/main/java/de/geheimagentnr1/dynamical_compass/handlers/ForgeRegistryEventHandler.java
@@ -1,9 +1,9 @@
 package de.geheimagentnr1.dynamical_compass.handlers;
 
 import de.geheimagentnr1.dynamical_compass.elements.commands.GiveDCCommand;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 
 
 @SuppressWarnings( "unused" )
@@ -12,8 +12,8 @@ public class ForgeRegistryEventHandler {
 	
 	
 	@SubscribeEvent
-	public static void handlerServerStartEvent( FMLServerStartingEvent event ) {
+	public static void handlerRegisterCommandsEvent( RegisterCommandsEvent event ) {
 		
-		GiveDCCommand.register( event.getCommandDispatcher() );
+		GiveDCCommand.register( event.getDispatcher() );
 	}
 }
