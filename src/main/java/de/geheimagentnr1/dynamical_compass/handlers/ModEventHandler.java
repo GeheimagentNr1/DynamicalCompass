eadb0305d423dceeab65681318e14acdf230a4ec
GeheimagentNr1 <geheimelache@hotmail.com>
2021-04-20 19:41:50 +0200

    Cleaned up code

--- src/main/java/de/geheimagentnr1/dynamical_compass/handlers/ModEventHandler.java
+++ src/main/java/de/geheimagentnr1/dynamical_compass/handlers/ModEventHandler.java
@@ -1,5 +1,6 @@
 package de.geheimagentnr1.dynamical_compass.handlers;
 
+import de.geheimagentnr1.dynamical_compass.DynamicalCompassMod;
 import de.geheimagentnr1.dynamical_compass.elements.items.ModItems;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
@@ -7,7 +8,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 
-@Mod.EventBusSubscriber( bus = Mod.EventBusSubscriber.Bus.MOD )
+@Mod.EventBusSubscriber( modid = DynamicalCompassMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD )
 public class ModEventHandler {
 	
 	
